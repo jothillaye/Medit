1dc8369f14bf522a8cd4bc3cb6a1f908aca0cb22
Shadowsun52 <jenicot.alexandre@gmail.com>
2013-11-30 10:18:59 +0100

    modification menu principale ajout du controller travailleur

--- Medit/Controllers/HomeController.cs
+++ Medit/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Medit.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "don't Modify this template, is it amazing.";
 
             return View();
         }
